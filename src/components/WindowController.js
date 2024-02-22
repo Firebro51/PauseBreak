d306afd76f0b9a08032eec8c1cf15770aa76c4ad
Firebro51 <craigf987@bellsouth.net>
2024-02-22 05:30:18 -0500

    updated centered SVG window buttons and more styling starting on hover effects

--- src/components/WindowController.js
+++ src/components/WindowController.js
@@ -9,11 +9,14 @@ import LogoIcon from '../images/Cool_Bird_Logo.svg';
 const WindowController = () => {
     return (
         <div className="window-controller">
-            <img src={LogoIcon} alt="Logo" className="logo" />
+            <div className="window-controller-left">
+                <img src={LogoIcon} alt="Logo" className="logo" />
+                <span className="app-name">PauseBreak</span>
+            </div>
             <div className="window-controls">
-                <img src={MinimizeIcon} alt="Minimize" onClick={() => window.electronAPI.minimize()} />
-                <img src={MaximizeIcon} alt="Maximize" onClick={() => window.electronAPI.maximize()} />
-                <img src={CloseIcon} alt="Close" onClick={() => window.electronAPI.close()} />
+                <img src={MinimizeIcon} alt="Minimize" className="minimize-btn" onClick={() => window.electronAPI.minimize()} />
+                <img src={MaximizeIcon} alt="Maximize" className="maximize-btn" onClick={() => window.electronAPI.maximize()} />
+                <img src={CloseIcon} alt="Close" className="close-btn" onClick={() => window.electronAPI.close()} />
             </div>
         </div>
     );
